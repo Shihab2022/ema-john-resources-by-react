b211a496fb8b2fda0e6b52605d9364420283db9b
Md Shihab Uddin <uddinmdshihab452@gmail.com>
2022-03-22 19:53:39 +0600

    compleate cart

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './Componant/Header/Header';
+import Shop from './Componant/Shop/Shop';
 
 function App() {
   return (
     <div>
      <Header></Header>
+     <Shop></Shop>
     </div>
   );
 }
